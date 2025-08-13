9eb598e1cff6ac20702fd4cfcb4cb7d37dc21710
Brian Parrish <brian.parrish3739@gmail.com>
2025-08-12 21:42:42 -0400

    register suspense fix

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,11 +1,14 @@
-import { RegisterForm } from "@/components/register-form"
+import { RegisterForm } from "@/components/register-form";
+import { Suspense } from "react";
 
 export default function Page() {
   return (
     <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10 bg-muted">
       <div className="w-full max-w-sm">
-        <RegisterForm />
+        <Suspense>
+          <RegisterForm />
+        </Suspense>
       </div>
     </div>
-  )
+  );
 }
